38a3cbd8aafbd4128fcdbbfd33ec62fa93100d94
Sedanur Çevik <sdnrcvk@gmail.com>
2021-03-13 04:07:48 +0300

    components/models/services/observable

---
+++ src/app/models/customer.ts
@@ -0,0 +1,7 @@
+export interface Customer{
+  id:number;
+  userId:number;
+  userName:string;
+  companyName:string;
+  email:string;
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,9 @@
+export interface Rental{
+  id:number;
+  carId:number;
+  brandName:string;
+  userName:string;
+  companyName:string;
+  rentDate:string;
+  returnDate:string;
+}
